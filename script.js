99a203ae2727b512d5122721ab574603912c609e
eceelif <140750622+eceelif@users.noreply.github.com>
2024-01-30 14:40:02 +0300

    current ve future birlikte

--- script.js
+++ script.js
@@ -1,144 +1,209 @@
-//https://www.weatherapi.com/
-//todo: tahmin -> ileri yönelik
-//todo: favori şehir eklemek (arge).
-//todo: api.weatherapi.com burdaki başka bir endpoint i eklemek.
-//todo: ilk yüklendiği zaman favori şehri gösterme
-//todo: ilk yüklendiği zaman querystringden ilgili şehri gösterme -> http://localhost:5500/?city=istanbul
-//todo: css ekleyelim... güzelleştirelim.
-//todo: open-meteo apisinden veri çekip başka bir div de gösterelim.
+// https://www.weatherapi.com/
+// todo: tahmin -> ileri yönelik
+// todo: favori şehir eklemek (ar-ge).
+// todo: api.weatherapi.com buradaki başka bir endpoint'i eklemek.
+// todo: ilk yüklendiği zaman favori şehri gösterme
+// todo: ilk yüklendiği zaman querystringden ilgili şehri gösterme -> http://localhost:5500/?city=istanbul
+// todo: css ekleyelim... güzelleştirelim.
 
-//globals
-let derece_dom;
-let lastUpdate_dom;
-let img_dom;
-let imgText_dom;
+//http://api.weatherapi.com/v1/forecast.json?key=c1fa599bff55488eb02211050242501&q=istanbul&days=5&aqi=yes&alerts=yes
+// globals
+let api_key= "c1fa599bff55488eb02211050242501"
+let forecastLength = -1;
 
-async function btnClick(event) {
-  let domTxtCity = document.getElementById("txtCity");
-  let city = domTxtCity.value;
+//let days=5;
+// ...
+//currentWeather: http://api.weatherapi.com/v1/current.json?key=c1fa599bff55488eb02211050242501&q=istanbul&aqi=no
 
-  if (city === "") {
-    alert("Boş şehir ismi girilemez");
-    return;
-  }
-
-  try {
-    let currentWeather = await weather_api.dtoFunctions.getCurrentWeather(city);
-    let futureWeather = await weather_api.dtoFunctions.getFutureWeather(city, 5);
-
-    let result = {
-      currentWeather: currentWeather,
-      futureWeather: futureWeather
+let weather_api={
+  getCurrentWeather: async function (city) {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
     };
 
-    generateCityFutureWeatherDom(result);
-    console.log(result);
+    try {
+        let result = await fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}&aqi=no\n`, requestOptions)
+            .then(response => response.text());
+        result = JSON.parse(result);
+        return result;
+    } catch (error) {
+        console.log("error", error);
+        return error;
+    }
+},
+//http://api.weatherapi.com/v1/forecast.json?key=c1fa599bff55488eb02211050242501&q=istanbul&days=1&aqi=yes&alerts=yes
+
+getForecastWeather: async function (city,days) {
+  var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+  };
+
+  try {
+      let result = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}&${days}=1&aqi=yes&alerts=yes`, requestOptions)
+          .then(response => response.text());
+      result = JSON.parse(result);
+      return result;
   } catch (error) {
-    console.error("Error fetching weather data:", error);
+      console.log("error", error);
+      return error;
+  }
+},
+
+  getFutureWeather: async function (city,days){
+    days = (days <= 0) ? 14 : days; 
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    try {
+      let result= await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}&days=${days}&aqi=yes&alerts=yes`, requestOptions)
+      .then(response => response.text());
+      result = JSON.parse(result);
+      return result;
+    } catch (error) {
+      console.log("error", error);
+      return error;
+    }
+    
+  },
+
+ dtoFunction: {
+  getCurrentWeather: async function(city){
+    let return_value = await weather_api.getCurrentWeather(city);
+    let result={
+      temp: return_value.current.temp_c,
+      img: return_value.current.condition.icon,
+      img_text: return_value.current.condition.text,
+      last_update:return_value.current.last_updated
+    }
+return result;
+
+  },
+  getFutureWeather: async function(city, days) {
+    let return_value = await weather_api.getFutureWeather(city, days);
+    let forecastDataArr = return_value.forecast.forecastday;
+    let resultArr = [];
+    for (let i = 0; i < forecastDataArr.length; i++) {
+      let tmp_data = {
+        next_date : forecastDataArr[i].date,
+        next_date_max_Temp : forecastDataArr[i].day.maxtemp_c
+      }
+      resultArr.push(tmp_data);
+    }
+    return resultArr;
+
+
   }
 }
 
+}
 
-async function btnClickFavoryCity(event) {
+async function btnClick(event) {
+  console.log("click");
   let domTxtCity = document.getElementById("txtCity");
   let city = domTxtCity.value;
   if (city === '') {
     alert(`Şehir ismi yazınız`);
   }
 
-  
-    
+    let result = await weather_api.dtoFunction.getFutureWeather(city, forecastLength);
+    generateCityFutureWeatherDom(result);
+    console.log(result);
+    let result_current = await weather_api.dtoFunction.getCurrentWeather(city);
+    generateCityDom(result_current);
+    console.log(result_current);
+
+
+    //console.log(result);
+    /*let json_weather_data = response_data;
+    console.log(json_weather_data);
+    let data1 = json_weather_data.forecast;
+    console.log(data1);
+  } catch (error) {
+    console.error('Error fetching future weather:', error);
+  }*/
 }
-
-/*function third_way(dtoCurrentWeatherObject) {
-  //put data in dom
-  derece_dom.innerText = dtoCurrentWeatherObject.temp + " derece";
-  lastUpdate_dom.innerText =
-    "Son Güncelleme : " + dtoCurrentWeatherObject.last_update;
-  img_dom.src = dtoCurrentWeatherObject.img;
-  imgText_dom.innerText = dtoCurrentWeatherObject.img_text;
-} */
-
-function generateHtml(dtoCurrentWeatherObject) {
-  let generatedDataDiv = document.getElementById("generatedData");
-  generatedDataDiv.innerHTML = "";
-
-  let derece_text = dtoCurrentWeatherObject.temp + " derece";
-  let last_update = "Son Güncelleme : " + dtoCurrentWeatherObject.last_update;
-  let temp = `
-    <div>${derece_text}</div>
-    <div>${last_update}</div>
-    <img src="${dtoCurrentWeatherObject.img}"/>
-    <div>${dtoCurrentWeatherObject.img_text}</div>`;
-
-  generatedDataDiv.innerHTML = temp;
-}
-
-function generateCityDom(dtoCurrentWeatherObject) {
-  //clear dom....
-  let generatedDataDiv = document.getElementById("generatedData");
-  generatedDataDiv.innerHTML = "";
-
-  //put data in dom
+function generateCityDom(dtoCurrentWeatherObject){
+  let appDiv=document.getElementById('generateData');
+  appDiv.innerHTML='';
   let dataTempDiv = document.createElement("div");
-  dataTempDiv.innerText = dtoCurrentWeatherObject.temp + " derece";
-  generatedDataDiv.appendChild(dataTempDiv);
-
-  let dataLastUpdateDiv = document.createElement("div");
-  dataLastUpdateDiv.innerText =
-    "Son Güncelleme : " + dtoCurrentWeatherObject.last_update;
-  generatedDataDiv.appendChild(dataLastUpdateDiv);
-
+      dataTempDiv.innerText= dtoCurrentWeatherObject.temp+ " derece";
+  appDiv.appendChild(dataTempDiv);
+  let datalast_updateDiv = document.createElement("div");
+      datalast_updateDiv.innerText= dtoCurrentWeatherObject.last_update;
+  appDiv.appendChild(datalast_updateDiv);
+  
   let dataImg = document.createElement("img");
-  dataImg.src = dtoCurrentWeatherObject.img;
-  dataImg.addEventListener("click", () => {
-    console.log("img clicked");
-  });
-  generatedDataDiv.appendChild(dataImg);
+      dataImg.src= dtoCurrentWeatherObject.img;
+  appDiv.appendChild(dataImg);
 
-  let dataImgTextDiv = document.createElement("div");
-  dataImgTextDiv.innerText = dtoCurrentWeatherObject.img_text;
-  generatedDataDiv.appendChild(dataImgTextDiv);
+  let dataImg_TextDiv = document.createElement("div");
+  dataImg_TextDiv.innerText= dtoCurrentWeatherObject.img_text;
+appDiv.appendChild(dataImg_TextDiv);
+
+
 }
-function generateCityFutureWeatherDom(dtoFutureWeatherObject) {
-  if (dtoFutureWeatherObject) {
-    let app_div = document.getElementById("app");
-    app_div.innerHTML = ""; // Clear previous content
 
-    for (let i = 1; i <= 5; i++) {
-      let dateKey = `next_date${i}`;
-      let maxTempKey = `next_date${i}_max_temp`;
+async function btnClickFavoryCity(event) {
+  let domFavoriteCity = document.getElementById("txtCity");
+  var cityName = document.getElementById("txtCity").value;
+  console.log("Adding city to favorites:", cityName);
+  if (cityName !== "") {
+    util.cookie.setCookie('favoriteCity',cityName);
 
-      let dataFutureTemDiv = document.createElement("div");
-      dataFutureTemDiv.innerText = `${dtoFutureWeatherObject[dateKey]} tarihinde maksimum hava sıcaklığı: ${dtoFutureWeatherObject[maxTempKey]} derece`;
-      app_div.appendChild(dataFutureTemDiv);
-    }
+    // Clear the input field
+    domFavoriteCity.value = "";
+  } else {
+    alert("Please enter a valid city name.");
   }
 }
 
+function generateCityFutureWeatherDom(dtoFutureWeatherObject) {
+  if (dtoFutureWeatherObject) {
+    let app_div = document.getElementById("app");
+    let tmp_Header_div = document.createElement("h3");
+      tmp_Header_div.innerText="Gelecek Hava Tahmini";
+      app_div.appendChild(tmp_Header_div);
 
-// function loadDataHelper(){
-//   if(typeof weather_api === 'undefined'){
-//     loadJS('dataHelper.js');
-//   }
-// }
+    for (let i = 0; i < dtoFutureWeatherObject.length; i++) {
+      const item = dtoFutureWeatherObject[i];
 
-document.addEventListener("DOMContentLoaded", async function () {
-  console.log("Dom loaded");
+      
 
-  derece_dom = document.getElementById("derece");
-  lastUpdate_dom = document.getElementById("lastUpdate");
-  img_dom = document.getElementById("img");
-  imgText_dom = document.getElementById("imgText");
+      let tmp_div = document.createElement("div");
+      tmp_div.innerText = `${item.next_date} tarihinde maksimum hava sıcaklığı: ${item.next_date_max_Temp} derece`;
+      app_div.appendChild(tmp_div);
+      
+    }
+    
+  }
+}
 
-  let json_weather_data = await weather_api.dtoFunctions.getFutureWeather("istanbul", 5);
-  let data1 = json_weather_data.forecast;
-  console.log(data1);
-  // let responseData = await openMeteo.getCurrrentIstanbulWeather();
-  // console.log(responseData);
+document.addEventListener('DOMContentLoaded', async () => {
+  console.log('Dom loaded');
+  
+    // let json_weather_data = await weather_api.getFutureWeather("istanbul", 5);
+    // let data1 = json_weather_data.forecast;
+    // console.log(data1);
+    // Pass the result to generateCityFutureWeatherDom
 
-  // weather_api.uzunluk => 10
-  //  let response_data = await weather_api.getForcastDataWeather("maltepe",3);
-  //  let json_weather_data = JSON.parse(response_data);
-  //  console.log(json_weather_data);
+
+    //let myfavoriteCity = util.cookie.getCookie('favoriteCity');
+    //let json_weather_data = await weather_api.getFutureWeather(myfavoriteCity, 5);
+    // let json_weather_data = await weather_api.dtoFunction.getFutureWeather(myfavoriteCity, forecastLength);
+    // generateCityFutureWeatherDom(json_weather_data)
+    // console.log(json_weather_data);
+    let response_data = await weather_api.dtoFunction.getCurrentWeather("istanbul");
+
+    console.log(response_data);
+
+    //let paramCity = util.queryString.getParam('city');
+    //console.log(paramCity);
+
+    //let json_weather_data = await weather_api.getFutureWeather(paramCity, forecastLength);
+    //generateCityFutureWeatherDom(json_weather_data)
+
+
 });
